04139574f1f820534379cd687db50fe3bda59308
TomaszNiznik <14608@eppuz.edu.pl>
2025-04-08 13:38:03 +0200

    Fix

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('car/<car_id>',views.car,name='car'),
-    path('category/<category_id>/', views.category, name='category')
+    path('', views.index, name='index'),
+    path('car/<car_id>/', views.car, name='car'),
+    path('category/<category_id>/', views.category, name='category'),
 ]
