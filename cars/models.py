bc8bf6392ed36bc780d9a6463ed524615721a366
TomaszNiznik <14608@eppuz.edu.pl>
2025-04-08 08:17:50 +0200

    Fix bugs and add first car

--- cars/models.py
+++ cars/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Equipment (models.Model):
     equipment= models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.equipment
 # Create your models here.
-class Car(models.Model):
+class Car(models.Model): 
     Engine_Types = [
         ("benzyna", "Benzynowy"),
         ("disel", "Disel"),
@@ -37,10 +40,10 @@ class Car(models.Model):
     ]
     
 
-    brand = models.CharField(max_lenght = 50)
-    model = models.CharField(max_lenght = 50)
-    engine_type= models.CharField(max_lenght=50, choices=Engine_Types)
-    gearbox = models.CharField(max_lenght=50, choices =GEARBOX_TYPES)
+    brand = models.CharField(max_length =  50)
+    model = models.CharField(max_length = 50)
+    engine_type= models.CharField(max_length=50, choices=Engine_Types)
+    gearbox_type = models.CharField(max_length=50, choices =GEARBOX_TYPES)
     engine_power = models.PositiveSmallIntegerField()
     engine_capacity = models.PositiveSmallIntegerField()
     seats_count = models.PositiveSmallIntegerField()
@@ -49,10 +52,11 @@ class Car(models.Model):
     color = models.CharField(max_length=50)
     body_type = models.CharField(max_length=50, choices=BODY_TYPES) 
     category = models.CharField(max_length=50, choices=CAR_CLASSES) 
-    fuel_usage = models.DecimalField
+    fuel_usage = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     price = models.DecimalField(max_digits =10 , decimal_places=2)
     mileage_limit = models.PositiveIntegerField()
     value = models.PositiveBigIntegerField()
     availability =models.BooleanField()
     insurance_expiry_date = models.DateField()
     equipment =models.ManyToManyField(Equipment)
+    image = models.ImageField()
